4cc2c9ada617d4b19ea979a0925f65707de8a025
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2021-08-06 15:06:58 +0530

    Update kmain.c
    Update kmain.c for Writing Text

--- kmain.c
+++ kmain.c
@@ -1,4 +1,27 @@
-int sum_of_three(int arg1, int arg2, int arg3)
+char *fb = (char *) 0x000B8000;
+
+
+
+void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
+
     {
-        return arg1 + arg2 + arg3;
+
+        fb[i] = c;
+
+        fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
+
     }
+
+    
+
+#define FB_GREEN     2
+
+#define FB_DARK_GREY 8    
+
+void kmain()
+
+{
+
+	fb_write_cell(0, 'A',FB_GREEN,FB_DARK_GREY);
+
+}
