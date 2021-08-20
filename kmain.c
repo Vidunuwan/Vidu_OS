2e739a781da7eb52b88ef5109c5206e046f0cdcd
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2021-08-20 10:25:30 -0400

    Add files via upload (#7)
    interupt_and_inputs

--- kmain.c
+++ kmain.c
@@ -1,17 +1,21 @@
   
     #include "frame_buffer.h"
     #include "serial_port.h"
+    #include "io.h"
     #include "memory_segments.h"
-
-
+    #include "interrupts.h"
+    #include "keyboard.h"
+    #include "pic.h"
     
-    int main(){
 
-           char ptr2[] = "Starting Vidu OS";
-   
-    
-    serial_write(0x3F8, ptr2, 16);
-    fb_write(ptr2, 16);
-    segments_install_gdt();
-    
-    }	
+    void kmain()
+    {
+
+       //char arr[] = "Startin Vidu OS";
+       //fb_move_cursor(6*80);
+       //fb_write(arr, 20);
+       //serial_write(arr, 20);
+       segments_install_gdt();
+       interrupts_install_idt();
+
+    }
