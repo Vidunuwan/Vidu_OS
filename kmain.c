653f44a3f769a8cf773a1ec1dccaafaf0dbc1e3f
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2021-08-27 10:50:32 -0400

    Add files via upload

--- kmain.c
+++ kmain.c
@@ -6,16 +6,37 @@
     #include "interrupts.h"
     #include "keyboard.h"
     #include "pic.h"
+    #include "multiboot.h"
     
-
-    void kmain()
+    
+    void kmain(unsigned int ebx)
     {
 
        //char arr[] = "Startin Vidu OS";
        //fb_move_cursor(6*80);
        //fb_write(arr, 20);
        //serial_write(arr, 20);
-       segments_install_gdt();
-       interrupts_install_idt();
+       //segments_install_gdt();
+       //interrupts_install_idt();
+       
+        multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+  	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+  	unsigned int address_of_module = modules->mod_start;
+  	
+  	if((mbinfo->mods_count) == 1){
+  		char message[] = "ONE module loaded successfully!";
+  		serial_write(message,sizeof(message));
+  		
+  		typedef void (*call_module_t)(void);
+        	/* ... */
+        	call_module_t start_program = (call_module_t) address_of_module;
+        	start_program();
+        	/* we'll never get here, unless the module code returns */
+
+  	}else{
+  		char message[] = "Error: More than ONE module loaded";
+  		serial_write(message,sizeof(message));
+  	}
 
     }
+    
