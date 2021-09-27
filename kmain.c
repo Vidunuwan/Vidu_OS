6e36a84c3b4a64ccbe88a67c155d6d53c10c2478
Umayanga Vidunuwan <73218580+Vidunuwan@users.noreply.github.com>
2021-09-27 23:55:58 +0530

    Add files via upload

--- kmain.c
+++ kmain.c
@@ -1,36 +1,27 @@
-  
-    #include "frame_buffer.h"
-    #include "serial_port.h"
-    #include "io.h"
-    #include "memory_segments.h"
-    #include "interrupts.h"
-    #include "keyboard.h"
-    #include "pic.h"
+ #include "drivers/frame_buffer.h"
+    #include "drivers/serial_port.h"
+    #include "drivers/io.h"
+    #include "segmentation/segments.h"
+    #include "interrupts/interrupts.h"
+    #include "interrupts/keyboard.h"
+    #include "interrupts/pic.h"
     #include "multiboot.h"
     #include "paging.h"
-    
-   void init(){
-   segments_install_gdt();
-   interrupts_install_idt();
-   init_paging();
-}
-    
-    
-    void kmain(unsigned int ebx)
-    {
-    
+    #include "type.h"
+    #include "kheap.h"
 
-       //char arr[] = "Startin Vidu OS";
-       //fb_move_cursor(6*80);
-       //fb_write(arr, 20);
-       //serial_write(arr, 20);
-       //segments_install_gdt();
-       //interrupts_install_idt();
-       
-       
-       init();
-       
-        multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+    void kmain(unsigned int ebx,u32int kernel_physical_start, u32int kernel_physical_end)
+    {
+
+	init_paging(kernel_physical_start,kernel_physical_end);
+       /*char arr[] = "Welcome to WVBOS";
+       fb_move_cursor(6*80);
+       fb_write(arr, 20);
+       serial_write(arr, 20);
+       segments_install_gdt();
+       interrupts_install_idt();*/
+     
+   multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
   	multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
   	unsigned int address_of_module = modules->mod_start;
   	
@@ -50,4 +41,3 @@
   	}
 
     }
-    
